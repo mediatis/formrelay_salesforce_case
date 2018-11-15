567588f9b59b19b52a1be47372449abffa15bda6
Meelis Karulin <karulin@mediatis.de>
2018-11-15 15:02:59 +0100

    Add signal that allows overriding the redirect URL

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,8 @@ if (!defined('TYPO3_MODE')) {
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['formrelay']['dataProcessor'][] = \Mediatis\FormrelaySalesforceCase\Hooks\SalesForceCase::class;
 
 $conf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['formrelay_salesforce_case']);
+
+// Override the default RedirectPostProcessor
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Form\PostProcess\RedirectPostProcessor::class] = [
+    'className' => \Mediatis\FormrelaySalesforceCase\PostProcessors\RedirectPostProcessor::class
+];
