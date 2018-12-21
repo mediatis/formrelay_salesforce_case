fe10c1e9877903e43d423cd70f2229a81a990655
Meelis Karulin <karulin@mediatis.de>
2018-12-21 18:04:08 +0100

    WIP:Bugfix

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,7 +3,7 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'formrelay_salesforce_case',
     'Configuration/TypoScript',
     'FormRelay SalesForce Case'
 );
