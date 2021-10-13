7382c809fa1b7da90e901599eeb8178b50237062
s-ude <ude@mediatis.de>
2021-10-14 01:50:07 +0200

    [FEATURE] Asynchronous Submissions
    * LMSCB-16: form-relay code updates: external library, async submissions
    
    * LMSCB-16: dependency updates

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,21 @@
 <?php
+
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function() {
-    $registry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\Mediatis\Formrelay\Service\Registry::class);
-    $registry->registerDestination(\Mediatis\FormrelaySalesforceCase\Destination\SalesforceCase::class);
+call_user_func(function () {
+    // relay initalization
+    \Mediatis\Formrelay\Utility\RegistrationUtility::registerInitialization(\Mediatis\FormrelaySalesforceCase\Initialization::class);
+
+    /** @var \TYPO3\CMS\Extbase\SignalSlot\Dispatcher $dispatcher */
+    $dispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
+
+    // configuration updater
+    $dispatcher->connect(
+        \Mediatis\Formrelay\Configuration\RouteConfigurationUpdaterInterface::class,
+        \Mediatis\Formrelay\Configuration\RouteConfigurationUpdaterInterface::SIGNAL_UPDATE_ROUTE_CONFIGURATION,
+        \Mediatis\FormrelaySalesforceCase\Configuration\ConfigurationUpdater::class,
+        'updateRouteConfiguration'
+    );
 });
